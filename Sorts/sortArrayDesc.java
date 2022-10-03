114b680a66062c38bd1cc2a44d8a9b67c271906f
iltonesteves <iltonesteves@hotmail.com>
2022-10-03 13:05:58 -0300

    sort
    Classificação

---
+++ Sorts/sortArrayDesc.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sorts;
+
+/**
+ *
+ * @author ghandi
+ */
+import java.util.Arrays;
+
+public class sortArrayDesc {
+
+	public static void main(String[] args) {
+		int[] array = {4, 74, 14, 9, 3, 263, -74, 44, -79, 611 , 19}; 
+		System.out.println("Current Array:"+Arrays.toString(array));
+		//Current Array:[4, 74, 14, 9, 3, 263, -74, 44, -79, 611, 19]
+		
+		 int temporary;
+	     for (int i = 0; i < array.length; i++) //we are looping every single element
+	        {
+	            for (int j = i + 1; j < array.length; j++) //and comparing with other array elements
+	            {
+	                if (array[i] < array[j])
+	                {
+	                	temporary =  array[i]; // keep the bigger value
+	                    array[i] = array[j]; // update i indexed array element
+	                    array[j] = temporary; // put back the value you have stored
+	                }
+	            }
+	        }
+ 
+	     System.out.println("Sorted Array: "+Arrays.toString(array));
+	     // Sorted Array: [611, 263, 74, 44, 19, 14, 9, 4, 3, -74, -79]
+	}
+
+}
